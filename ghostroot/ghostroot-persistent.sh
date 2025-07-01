25e61cd8f8f989e87891a99723c857b5e047fa7c
Skorpion96 <39702358+Skorpion96@users.noreply.github.com>
2025-07-01 02:44:48 +0200

    Update ghostroot-persistent.sh

--- ghostroot/ghostroot-persistent.sh
+++ ghostroot/ghostroot-persistent.sh
@@ -1,12 +1,12 @@
 #!/system/bin/sh
 GETPROP=$(getprop ro.product.device)
-sendroot() {
+ghostroot() {
 printf "$GETPROP:/ # "
 read CMD
 echo "$CMD" > /sdcard/rootbridge/in/command.txt
 sleep 0.4
 cat /sdcard/rootbridge/out/result.txt
-sendroot
+ghostroot
 }
 
-sendroot
+ghostroot
