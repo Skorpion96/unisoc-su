f58c94120a8b7b3a522a08d5ff49ccf14def6dbc
Skorpion96 <39702358+Skorpion96@users.noreply.github.com>
2025-07-06 19:21:06 +0200

    Create ghostsu.sh

---
+++ ghostroot/ghostsu.sh
@@ -0,0 +1,26 @@
+#!/system/bin/env sh
+
+BR_IN="/sdcard/rootbridge/in/command.txt"
+BR_OUT="/sdcard/rootbridge/out/result.txt"
+
+usage() {
+  cat <<EOF >&2
+Usage: ghostsu -c <command>
+Example: ghostsu -c "id; ls /data"
+EOF
+  exit 1
+}
+
+if [ "$1" = "-c" ]; then
+  shift
+  [ $# -gt 0 ] || usage
+  CMD="$*"
+else
+  usage
+fi
+
+printf '%s\n' "$CMD" >"$BR_IN"
+
+sleep 0.2
+
+cat "$BR_OUT"
