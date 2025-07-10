b24c7481ba9b423e4ee851548291d29e8129dc7b
Skorpion96 <39702358+Skorpion96@users.noreply.github.com>
2025-07-10 16:24:28 +0200

    unified ghostroot and ghostsu
    unified ghostroot and ghostsu

--- ghostroot/ghostroot-1cmd.sh
+++
@@ -1,7 +0,0 @@
-#!/system/bin/sh
-GETPROP=$(getprop ro.product.device)
-printf "$GETPROP:/ # "
-read CMD
-echo "$CMD" > /sdcard/rootbridge/in/command.txt
-sleep 0.4
-cat /sdcard/rootbridge/out/result.txt
