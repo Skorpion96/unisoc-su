45dc7553e2edc3466273a3bccc10f43fa67129e2
Skorpion96 <39702358+Skorpion96@users.noreply.github.com>
2025-08-03 19:23:24 +0200

    Update unisoc-su-adb-shizuku-tut.sh

--- unisoc-su shell scripts/unisoc-su-adb-shizuku-tut.sh
+++ unisoc-su shell scripts/unisoc-su-adb-shizuku-tut.sh
@@ -1,6 +1,6 @@
 #!/system/bin/sh
 
-echo "On EngineerMode Adb Shell Activity enter the full cli-pie PATH including the cli-pie applet, don't press start, prepare 'rish -c "setprop persist.sys.cmdservice.enable enable"' or 'adb shell setprop persist.sys.cmdservice.enable enable' then press enter on this setprop shell, as fast as possible go back to the EngineerMode Activity you prepared first and press start, then press enter on this script shell to continue..."
+echo "On EngineerMode Adb Shell Activity enter the full cli-pie PATH including the cli-pie applet, don't press start, prepare 'rish -c "setprop persist.sys.cmdservice.enable enable"' or 'adb shell setprop persist.sys.cmdservice.enable enable' then press enter on this setprop shell, as fast as possible go back to the EngineerMode Activity you prepared first and press start, it should tell 'Connected to the server', then press enter on this script to continue..."
 read -n 1 -s
 
 echo "run these commands:'nc 127.0.0.1' or run the UnisocEngsyshell for your system, then 'source /sdcard/tools.sh' and then 'cli-pie', this must be done manually as the script would drop after entering the system shell"
