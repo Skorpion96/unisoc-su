b1d047f6bd6f4b62a7a51fb9ec85734192cdcdcd
Skorpion96 <39702358+Skorpion96@users.noreply.github.com>
2025-08-03 19:22:51 +0200

    Update unisoc-su-syshell-only-tut.sh

--- unisoc-su shell scripts/unisoc-su-syshell-only-tut.sh
+++ unisoc-su shell scripts/unisoc-su-syshell-only-tut.sh
@@ -1,24 +1,6 @@
 #!/system/bin/sh
 
-# Find the correct app lib path even with Scoped Storage naming
-SYS_TOOLS=$(find /data/app -type d -path "*/com.sammy.systools*/lib/arm64" 2>/dev/null | head -n1)
-
-# Fallback if not found
-if [ -z "$SYS_TOOLS" ]; then
-  echo "Error: com.sammy.systools library path not found, install the app before running this script."
-  exit 1
-fi
-
-# Set environment variables
-export TERMINFO=/sdcard/terminfo
-export TERM=xterm-256color
-export PATH="$PATH:$SYS_TOOLS"
-
-echo "Go on EngineerMode Adb Shell activity, on the first shell prompt input 'setprop persist.sys.cmdservice.enable enable', don't press start, go on the second activity and paste the full cli-pie PATH including the binary, go to the setprop activity, press start now, then go on the cli-pie activity and press start as fast as possible, it should tell 'Connected to the server', after that press enter on this script to get a root shell on cmd_services"
+echo echo "Go on EngineerMode Adb Shell activity, on the first shell prompt input 'setprop persist.sys.cmdservice.enable enable', don't press start, go on the second activity and paste the full cli-pie PATH including the binary, go to the setprop activity, press start now, then go on the cli-pie activity and press start as fast as possible, it should tell 'Connected to the server', after that press end on the first setprop activity and delete the content, then enter the reverse shell command for your system on the setprop activity and press start, then press enter on this script to continue..."
 read -n 1 -s
 
-echo "Welcome to the Unisoc-SU Shell (aka CVE-2022-47339 reborn in 2025)\n"
-
-cli-pie
-
-echo "Unisoc-SU Shell provided by the Elite x Skorpion96"
+echo "run these commands:'nc 127.0.0.1' or run the UnisocEngsyshell for your system, then 'source /sdcard/tools.sh' and then 'cli-pie', this must be done manually as the script would drop after entering the system shell"
