ae57059a622d1dedfcbfb176a79902e240f47002
Skorpion96 <39702358+Skorpion96@users.noreply.github.com>
2025-09-15 20:53:41 +0200

    Update rootbridge.sh
    Now using this updated rootbridge and the c version of the ghostroot will make impossible to crash the bridge

--- ghostroot/rootbridge.sh
+++ ghostroot/rootbridge.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
 if [ "$(whoami)" != "root" ]; then
     echo "This script must be run as the root user (UID 0) on the cmd_services shell, run unisoc-su before running this script."
     exit 1
@@ -7,14 +6,20 @@ fi
 
 HOME=/
 if [ -s /sdcard/rootbridge ]; then
-cmd=$(rm -r /sdcard/rootbridge)
+    rm -r /sdcard/rootbridge
 fi
 mkdir -p /sdcard/rootbridge/in /sdcard/rootbridge/out
-while true; do
-  if [ -s /sdcard/rootbridge/in/command.txt ]; then
-    cmd=$(cat /sdcard/rootbridge/in/command.txt)
+execute_in_channel() {
+    local cmd="$1"
     echo "[root executing] $cmd"
     eval "$cmd" > /sdcard/rootbridge/out/result.txt 2>&1
-    rm /sdcard/rootbridge/in/command.txt
-  fi
+    return 0
+}
+while true; do
+    if [ -s /sdcard/rootbridge/in/command.txt ]; then
+        cmd=$(cat /sdcard/rootbridge/in/command.txt)
+        execute_in_channel "$cmd"
+        
+        rm /sdcard/rootbridge/in/command.txt
+    fi
 done
