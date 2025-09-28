1cac4415a866541a790c21fe9736293d4fd76cff
Skorpion96 <39702358+Skorpion96@users.noreply.github.com>
2025-09-28 14:11:23 +0200

    Update unisoc-su-other-sockets.sh

--- unisoc-su shell scripts/unisoc-su-other-sockets.sh
+++ unisoc-su shell scripts/unisoc-su-other-sockets.sh
@@ -11,12 +11,9 @@ SYS_TOOLS=$(find /data/app -type d \( -path "*/com.sammy.systools*/lib/arm64" -o
 # Fallback if not found
 if [ -z "$SYS_TOOLS" ]; then
   echo "Error: com.sammy.systools library path not found, install the app before running this script."
-  exit 1
+  return 0
 fi
 
-# Set environment variables
-export TERMINFO=/sdcard/terminfo
-export TERM=xterm-256color
 export PATH="$PATH:$SYS_TOOLS"
 
 echo -n "Please input the socket you want to connect to: "
