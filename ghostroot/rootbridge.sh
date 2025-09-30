99b05c69c70326bf089b9009a4f07a63f5c23321
Skorpion96 <39702358+Skorpion96@users.noreply.github.com>
2025-09-30 17:18:33 +0200

    Update rootbridge.sh

--- ghostroot/rootbridge.sh
+++ ghostroot/rootbridge.sh
@@ -16,6 +16,10 @@ execute_in_channel() {
     echo "We don't do that here." > /sdcard/rootbridge/out/result.txt
         return 1
     fi
+    if echo "$cmd" | grep -qE '^exit [0-9]'; then
+    echo "We don't do that here." > /sdcard/rootbridge/out/result.txt
+        return 1
+    fi
     eval "$cmd" > /sdcard/rootbridge/out/result.txt 2>&1
     return 0
 }
