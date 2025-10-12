439d60aecd539ee718ee5a77ac2880e8faf212a5
Skorpion96 <39702358+Skorpion96@users.noreply.github.com>
2025-10-12 23:01:50 +0200

    added socat

---
+++ unisoc-su shell scripts/unisoc-su-socat-other-sockets.sh
@@ -0,0 +1,30 @@
+#!/system/bin/sh
+
+if [ "$(whoami)" != "system" ]; then
+    echo "This script must be run as the system user (UID 1000), run the reverse shell on the com.sprd.engineermode app before running this script."
+    exit 1
+fi
+TMPDIR=/sdcard
+cd /sdcard
+
+# Try to glob the directory
+found=""
+SYS_TOOLS="/data/app/com.sammy.systools*/lib/arm* /data/app/*/com.sammy.systools*/lib/arm*"
+for dir in $SYS_TOOLS; do
+    if [ -d "$dir" ]; then
+        export PATH="$dir:$PATH"
+        found=1
+        break
+    fi
+done
+
+# Fallback if not found
+if [ -z "$found" ]; then
+  echo "Error: com.sammy.systools library path not found, install the app before running this script."
+  return 0
+fi
+
+echo -n "Please input the socket you want to connect to: "
+read usercmd
+socat - ABSTRACT-CONNECT:$usercmd
+echo "Connection provided by the Elite x Skorpion96"
