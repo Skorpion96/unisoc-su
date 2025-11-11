9467c3b3e8c50a0297dacb5fba3ac22ded77add9
Skorpion96 <39702358+Skorpion96@users.noreply.github.com>
2025-11-11 01:34:56 +0100

    Add files via upload

---
+++ ghostroot/bridge_ftp.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+HOST="127.0.0.1"
+PORT="1234"
+EMODEDIR="/sdcard/Android/media/com.sprd.engineermode"
+INFILE="$EMODEDIR/rootbridge/in/command.txt"
+INDIR="$EMODEDIR/rootbridge/in"
+OUTDIR="$EMODEDIR/rootbridge/out"
+OUTFILE="$OUTDIR/result.txt"
+NC="/system/bin/toybox nc"
+SLEEP_INTERVAL=0.5
+if echo | $NC "$HOST" "$PORT" >/dev/null 2>&1; then
+    echo "Welcome to the Unisoc Eng Mode App Reverse Shell"
+    echo "Waiting for remote FTP Commands"
+fi
+rm -r $EMODEDIR && mkdir $EMODEDIR
+mkdir -p "$INDIR" "$OUTDIR"
+is_dangerous_content() {
+    echo "$cmd_text" | grep -qE 'eval'
+}
+while true; do
+    if [ -s "$INFILE" ]; then
+        cmd_text="$(cat "$INFILE")"
+        echo "[bridge executing] $cmd_text"
+        if is_dangerous_content "$cmd_text"; then
+            printf 'We do not allow eval/command-substitution or dangerous tokens in command.txt\n' > "$OUTFILE"
+        return 0
+        else
+            printf '%s\n' "$cmd_text" | $NC "$HOST" "$PORT" > "$OUTFILE" 2>&1 || {
+                printf 'Error: network/send failed (nc exit code: %s)\n' "$?" >> "$OUTFILE"
+            }
+        fi
+        rm -f "$INFILE"
+    fi
+    sleep "$SLEEP_INTERVAL"
+done
+
