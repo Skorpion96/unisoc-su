e291607a191f79cff8e88b3cb56e8f8d73f78872
Skorpion96 <39702358+Skorpion96@users.noreply.github.com>
2026-04-20 17:31:57 +0200

    Refactor rootbridge.sh to use BRIDGEHOME variable

--- ghostroot/rootbridge.sh
+++ ghostroot/rootbridge.sh
@@ -3,27 +3,27 @@ if [ "$(whoami)" != "root" ]; then
     echo "This script must be run as the root user (UID 0) on the cmd_services shell, run unisoc-su before running this script."
     return 0
 fi
-
+BRIDGEHOME=/sdcard/Android/media/.rootbridge
 HOME=/
-if [ -s /sdcard/rootbridge ]; then
-    rm -r /sdcard/rootbridge
+if [ -s $BRIDGEHOME ]; then
+    rm -r $BRIDGEHOME
 fi
-mkdir -p /sdcard/rootbridge/in /sdcard/rootbridge/out
+mkdir -p $BRIDGEHOME/in $BRIDGEHOME/out
 execute_in_channel() {
     local cmd="$1"
     echo "[root executing] $cmd"
     if echo "$cmd" | grep -qE 'eval'; then
-    echo "We don't do that here." > /sdcard/rootbridge/out/result.txt
+    echo "We don't do that here." > $BRIDGEHOME/out/result.txt
         return 1
     fi
-    eval "$cmd" > /sdcard/rootbridge/out/result.txt 2>&1
+    eval "$cmd" > $BRIDGEHOME/out/result.txt 2>&1
     return 0
 }
 while true; do
-    if [ -s /sdcard/rootbridge/in/command.txt ]; then
-        cmd=$(cat /sdcard/rootbridge/in/command.txt)
+    if [ -s $BRIDGEHOME/in/command.txt ]; then
+        cmd=$(cat $BRIDGEHOME/in/command.txt)
         execute_in_channel "$cmd"
         
-        rm /sdcard/rootbridge/in/command.txt
+        rm $BRIDGEHOME/in/command.txt
     fi
 done
