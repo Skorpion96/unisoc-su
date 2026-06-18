a3f56da4a90affd87e35fc2d7db80c6048de5b64
Skorpion96 <39702358+Skorpion96@users.noreply.github.com>
2026-06-18 19:03:51 +0200

    Update MainActivity.java

--- apks/unisoc-su poc/app/src/main/java/com/unisoc/su/MainActivity.java
+++ apks/unisoc-su poc/app/src/main/java/com/unisoc/su/MainActivity.java
@@ -1,6 +1,22 @@
 package com.unisoc.su;
 
 /*
+ * Copyright (C) 2007 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
  * Dipendenze richieste in app/build.gradle:
  *
  *   repositories { maven { url 'https://jitpack.io' } }
@@ -31,6 +47,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import jackpal.androidterm.emulatorview.ColorScheme;
 import jackpal.androidterm.emulatorview.EmulatorView;
 import jackpal.androidterm.emulatorview.TermSession;
+//
 
 import java.io.IOException;
 import java.io.File;
